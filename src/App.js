3f9eb7237397c65cceba34891a671d4a39bcb638
Jitendra Singh <jitendra@ukpa.co.uk>
2024-04-02 07:04:19 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import RichTextExample from './RichText';
+import { jsPDF } from "jspdf";
+import { SlateToReact } from '@slate-serializers/react'
 
 function App() {
+  const [value, setValue] = useState([])
+  const generatePDF =(e)=> {
+
+    console.log("test")
+    // Create a new jsPDF instance
+    const pdf = new jsPDF('p','pt','a4');
+
+    // Get the HTML content
+    pdf.setFontSize(12);
+    const element = SlateToReact(value)
+    console.log(element)
+    // Convert HTML to PDF
+    // pdf.html(element, {
+    //   callback: function (pdf) {
+    //     // Save the PDF
+    //    // pdf.save('output.pdf');
+    //   }
+    // });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onclick={generatePDF()}>Generate PDF</button>
+      <RichTextExample value={value} setValue={setValue}/>
     </div>
   );
 }
